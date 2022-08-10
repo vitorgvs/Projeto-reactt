6acd31de3a9fd6b3fbd76f205174504a9a2b8dca
vitor <vitorvilarsilva@gmail.com>
2022-08-10 12:50:23 -0300

    first commit

---
+++ reactsite/src/pages/home/index.jsx
@@ -0,0 +1,64 @@
+import React, {useState, useEffect} from 'react';
+import './style.css';
+import { Card } from '../../componentes/card' ;
+
+function App() {
+
+  const[studentName, setStudentName] = useState('');
+  const [students, setStudents] = useState([]);
+  const[user, setUser] = useState ({name: '', avatar:''})
+  function handleAddStudent(){
+    const newStudent = {
+      name: studentName,
+      time: new Date().toLocaleTimeString("pt-br",{
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+
+      })
+    };
+    setStudents(prevState => [...prevState, newStudent]);
+
+  }
+  
+useEffect(() =>{
+  fetch('https://api.github.com/users/vitorgvs')
+  .then(Response => Response.json())
+  .then(data => {setUser({
+name: data.name,
+avatar: data.avatar_url
+
+  })})
+ }, [] )
+
+  
+  return (
+    <div className="container">
+    <header>
+    <h1>{user.name}</h1>
+    <div>
+    <strong>Vitor Gabriel Vilar</strong>
+    <img src={user.avatar} alt= "foto de perfil" />
+    </div>
+    </header>
+    
+
+    <input 
+      type="text" 
+      placeholder="Digite o nome..."
+      onChange={e => setStudentName(e.target.value)} 
+    />
+    <button type="button" onClick={handleAddStudent}>Adcionar</button>
+    {
+    students.map(student => (<Card 
+     key={student.time}
+      name={student.name} 
+      time={student.time}/>))
+    
+    }
+    </div>
+    
+  )
+}
+
+export default App
